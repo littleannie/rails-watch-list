194a481e672d33bea313027d904acd4a46205b53
Anne Ly <anne.ly59@gmail.com>
2022-11-17 17:54:17 +0100

    rails watch list before front

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -20,10 +20,15 @@ class ListsController < ApplicationController
     end
   end
 
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to lists_path, status: :see_other
+  end
+
   private
 
   def list_params
     params.require(:list).permit(:name)
   end
 end
-
